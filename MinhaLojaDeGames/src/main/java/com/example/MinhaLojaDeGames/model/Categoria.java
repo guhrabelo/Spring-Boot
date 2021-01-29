a0dff9c0965e0efd440963481f8c160035ab2cb4
guhrabelo <gustavo.rabelo.teles01@gmail.com>
2021-01-29 14:50:22 -0300

    correção

--- MinhaLojaDeGames/src/main/java/com/example/MinhaLojaDeGames/model/Categoria.java
+++ MinhaLojaDeGames/src/main/java/com/example/MinhaLojaDeGames/model/Categoria.java
@@ -1,12 +1,17 @@
 package com.example.MinhaLojaDeGames.model;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name = "categoria")
@@ -17,12 +22,14 @@ public class Categoria {
 	private long id;
 
 	@NotNull
-	@Size(min = 5, max = 30)
 	private String nome;
 
 	@NotNull
-	@Size(min = 5, max = 100)
 	private String descricao;
+	
+	@OneToMany (mappedBy = "categoria", cascade = CascadeType.ALL)
+	@JsonIgnoreProperties ("categoria")
+	private List<Produto> produto;
 
 	public long getId() {
 		return id;
